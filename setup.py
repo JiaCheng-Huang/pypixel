e20b68af340b9bdd8f9e97f72ee8d05f97cb9340
dylan <dylan.jiacheng.huang@outlook.com>
2019-09-29 11:06:20 +0800

    Update version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="pypixel",
-    version="1.0.0",
+    version="1.0.1",
     author="Jiacheng Huang",
     author_email="Dylan.JiaCheng.Huang@outlook.com",
     description="A Python Library for Image Processing",
